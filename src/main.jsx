830ef0dece59b2573e8636e743dd4ea0276beb9d
pranjal gupta <pranjalgupta0195@gmail.com>
2026-03-25 20:01:07 +0530

    store patrh fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
